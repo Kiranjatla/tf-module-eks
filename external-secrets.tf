7291f865eae2294229d1df60edda54fb8970f64d
KIRAN <ks@gmail.com>
2025-11-02 11:34:13 +0530

    Kubernetes infra

--- external-secrets.tf
+++ external-secrets.tf
@@ -69,30 +69,30 @@ data "aws_iam_policy_document" "external-secrets-policy_document" {
 }
 
 
-data "aws_iam_policy_document" "external-secrets-policy_document" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    condition {
-      test = "StringEquals"
-      variable = "${replace(
-        aws_eks_cluster.eks.identity[0].oidc[0].issuer,
-        "https://",
-        "",
-      )}:aud"
-      values = ["sts.amazonaws.com"]
-    }
-
-    principals {
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(
-        aws_eks_cluster.eks.identity[0].oidc[0].issuer,
-        "https://",
-        "",
-      )}"]
-      type = "Federated"
-    }
-  }
-}
+#data "aws_iam_policy_document" "external-secrets-policy_document" {
+#  statement {
+#    actions = ["sts:AssumeRoleWithWebIdentity"]
+#
+#    condition {
+#      test = "StringEquals"
+#      variable = "${replace(
+#        aws_eks_cluster.eks.identity[0].oidc[0].issuer,
+#        "https://",
+#        "",
+#      )}:aud"
+#      values = ["sts.amazonaws.com"]
+#    }
+#
+#    principals {
+#      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(
+#        aws_eks_cluster.eks.identity[0].oidc[0].issuer,
+#        "https://",
+#        "",
+#      )}"]
+#      type = "Federated"
+#    }
+#  }
+#}
 
 resource "aws_iam_role" "external-secrets-oidc-role" {
   name               = "external-secrets-role-with-oidc"
