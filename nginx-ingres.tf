1c6361e223738b009774c330357b37443373a84d
KIRAN <ks@gmail.com>
2025-11-05 00:53:14 +0530

    frontend helm

--- nginx-ingres.tf
+++ nginx-ingres.tf
@@ -6,9 +6,9 @@ resource "null_resource" "nginx-ingress-chart" {
   count      = var.CREATE_NGINX_INGRESS ? 1 : 0
   provisioner "local-exec" {
     command = <<EOF
-helm repo add nginx-stable https://helm.nginx.com/stable
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm upgrade -i ingress nginx-stable/nginx-ingress -f ${path.module}/extras/nginx-ingress-values.yml
+helm upgrade -i ingress ingress-nginx/ingress-nginx -f ${path.module}/extras/nginx-ingress-values.yml
 EOF
   }
 }
